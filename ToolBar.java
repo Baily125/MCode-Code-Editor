50697d8cd807dea2373ad43f2cfd08130cc6d237
Baily125 <obcypro@gmail.com>
2024-09-28 14:35:59 +0200

    ToolBar.java

--- ToolBar.java
+++ ToolBar.java
@@ -4,38 +4,33 @@ import javax.swing.border.EmptyBorder;
 
 public class ToolBar {
     public ToolBar(JFrame frame) {
-        // Tworzenie toolbara
+    
         JToolBar toolBar = new JToolBar() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2.setColor(new Color(45, 45, 45)); // Ciemny kolor tła
-                g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15); // Zaokrąglone rogi
+                g2.setColor(new Color(45, 45, 45)); 
+                g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15); 
             }
         };
         
-        // Ustawienie preferowanego rozmiaru
         toolBar.setPreferredSize(new Dimension(400, 40));
-        toolBar.setFloatable(false); // Zablokowanie możliwości przeciągania
+        toolBar.setFloatable(false); 
         
-        // Ustawienie odstępów
-        toolBar.setBorder(new EmptyBorder(5, 5, 5, 5)); // Padding wewnętrzny
+        toolBar.setBorder(new EmptyBorder(5, 5, 5, 5)); 
         
-        // Dodanie przycisku
         ImageIcon runIcon = new ImageIcon("./media/runIcon.png");
         JButton runButton = new JButton(runIcon);
         runButton.setToolTipText("Run program");
         
-        // Stylizacja przycisku
-        runButton.setBackground(new Color(70, 70, 70)); // Kolor przycisku
-        runButton.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); // Usuń ramkę przycisku
-        runButton.setFocusPainted(false); // Usuń obrys przy skupieniu
+        runButton.setBackground(new Color(70, 70, 70)); 
+        runButton.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); 
+        runButton.setFocusPainted(false); 
 
         toolBar.add(runButton);
 
-        // Dodanie toolbara do ramki
         frame.add(toolBar, BorderLayout.NORTH);
     }
 }
